1b548f2c1e4e2b057af32ef0ef32e3528751a505
Josh Zizmor <joshzizmor@gmail.com>
2016-06-07 14:58:35 -0400

    sorting method for words fixed for ascending. Time difference is correctly calculated

--- app/controllers/applicationController.js
+++ app/controllers/applicationController.js
@@ -4,19 +4,20 @@ $(document).ready(function(){
 
   app.articleInfo.adapter.getBy().then(function(data){
     app.articleInfo.controller.render(data);
-   })
+  });
 
 
    $('.load-button').on('click', function(e){
      e.preventDefault();
-      console.log('clicked');
+      console.log('clicked load more');
       if (numberDisplayed < 29) {
-
           var arrayToPrint = app.articleInfo.adapter.getMore(numberDisplayed);
           app.articleInfo.controller.renderMore(arrayToPrint);
           numberDisplayed = numberDisplayed + 10;
-      }
 
+          var taste = localStorage.getItem('sortingType');
+          console.log(taste)
+      }
       else if (numberDisplayed < 59)
       {
       app.articleInfo.adapter.loadSecondFile().then(function(data){
@@ -27,9 +28,20 @@ $(document).ready(function(){
       }
       else {
         // change button
+        $('.load-button').text('No more articles to load').css("background-color",'#AD3A4B');
       }
+   });
 
-   })
+            $('#words-box').on('click', function(e){
+                e.preventDefault();
+                $("#table-id > tbody").empty();
+
+                localStorage.setItem('sortingType','Words');
+
+                app.articleInfo.controller.sort(numberDisplayed);
+            });
+
+
 
 
 
--- app/controllers/articleInfoController.js
+++ app/controllers/articleInfoController.js
@@ -6,12 +6,6 @@ app.articleInfo.controller = {
       for (i = 0; i < 10; i++) {
 
         var publishedTime = Date.parse(articles[i].publishDate);
-        var timeNow = Date.now();
-        var timeDifference = timeNow - publishedTime;
-        var diffInMinutes = Math.round(((timeDifference % 86400000) % 3600000) / 60000);
-        var diffHrs = Math.round((timeDifference % 86400000) / 3600000);
-        //http://stackoverflow.com/questions/7709803/javascript-get-minutes-between-two-dates
-
 
           $('#first-row').append(
               `<tr><td class="image-box"><img src="` + articles[i].image + `" width="50" height="50"></td>
@@ -19,7 +13,7 @@ app.articleInfo.controller = {
               <td class="name-box">` + articles[i].profileFirstName + ` `
               + articles[i].profileLastName + `</td>
               <td class="words-box">` + articles[i].words + `</td>
-              <td class="time-box">` + diffInMinutes + ` hours ago` + `</td></tr>`
+              <td class="time-box">` + app.articleInfo.controller.parseTime(publishedTime) + `</td></tr>`
           );
       }
 
@@ -27,18 +21,69 @@ app.articleInfo.controller = {
 
   renderMore: function(dataToPrint) {
     for (j = 0; j < 10; j++){
-      
+      var publishedTime = Date.parse(dataToPrint[j].publishDate);
+
       $('#first-row').append(
           `<tr><td class="image-box"><img src="` + dataToPrint[j].image + `" width="50" height="50"></td>
           <td class="title-box"><a href="` + dataToPrint[j].url + `">` + dataToPrint[j].title + `</a></td>
           <td class="name-box">` + dataToPrint[j].profileFirstName + ` `
           + dataToPrint[j].profileLastName + `</td>
           <td class="words-box">` + dataToPrint[j].words + `</td>
-          <td class="time-box"> 6 hours ago` + `</td></tr>`
+          <td class="time-box">` + app.articleInfo.controller.parseTime(publishedTime) + `</td></tr>`
       );
-
-
     }
+
+  },
+
+  sort: function(lastObject){
+
+        var allArticles = app.articleInfo.all;
+        var articlesToSort = [];
+
+        for( k = 0; k <= lastObject; k++ ){
+          articlesToSort.push(allArticles[k])
+        }
+
+        var sortedObj = [];
+        sortedObj = _.sortBy(articlesToSort, 'words');
+
+
+        for (l = 0; l < 10; l++){
+          var publishedTime = Date.parse(sortedObj[l].publishDate);
+
+          $('#first-row').append(
+              `<tr><td class="image-box"><img src="` + sortedObj[l].image + `" width="50" height="50"></td>
+              <td class="title-box"><a href="` + sortedObj[l].url + `">` + sortedObj[l].title + `</a></td>
+              <td class="name-box">` + sortedObj[l].profileFirstName + ` `
+              + sortedObj[l].profileLastName + `</td>
+              <td class="words-box">` + sortedObj[l].words + `</td>
+              <td class="time-box">` + app.articleInfo.controller.parseTime(publishedTime) + `</td></tr>`
+          );
+        }
+  },
+
+  parseTime: function(publishedTime) {
+   
+   var publishedTime = new Date( publishedTime ).getTime();
+   var now = new Date().getTime();
+
+
+   if (publishedTime < now) {
+       var milisec_diff = now - publishedTime;
+   }
+   else{
+       var milisec_diff = publishedTime - now;
+   }
+
+   var days = Math.floor(milisec_diff / 1000 / 60 / (60 * 24));
+   var date_diff = new Date( milisec_diff );
+
+   if(days > 365){
+     var years = Math.floor(days/365);
+     return years + " years ago"}
+   else if(days > 1 ) { return days + " days ago"}
+   else if (hours >= 1) {return date_diff.getHours() + " hours ago"}
+   else { return date_diff.getMinutes() + " minutes ago"}
 
 
   }
