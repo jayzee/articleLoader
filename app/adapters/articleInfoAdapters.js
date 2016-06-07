65e035b43ac01cb8a2e37d8b8f83e72e34dce783
Josh Zizmor <joshzizmor@gmail.com>
2016-06-07 12:11:49 -0400

    ability to add data from both sheets

--- app/adapters/articleInfoAdapters.js
+++ app/adapters/articleInfoAdapters.js
@@ -2,10 +2,9 @@ app.articleInfo.adapter = {
   getBy: function() {
     return  $.getJSON( "data/articles.json", function(data) {
 
-    })
-      .done(function(data) {
+    }).done(function(data) {
+
         var articles = data;
-        // console.log(data );
         articles.forEach(function(article){
 
         var articleInfo = new app.articleInfo.new(article.id, article.image, article.profile.first_name, article.profile.last_name, article.publish_at, article.words, article.title, article.url);
@@ -16,7 +15,20 @@ app.articleInfo.adapter = {
       .fail(function() {
         console.log( "error" );
       });
+  },
 
+  loadSecondFile: function() {
+
+    return $.getJSON( "data/more-articles.json", function(moreData) {
+    }).done(function(moreData){
+        var moreArticles = moreData;
+
+        moreArticles.forEach(function(article){
+            new app.articleInfo.new(article.id, article.image, article.profile.first_name, article.profile.last_name, article.publish_at, article.words, article.title, article.url);
+        })
+
+
+    });
   },
 
   getMore: function(startingNumber){
@@ -24,12 +36,10 @@ app.articleInfo.adapter = {
     var arrayToPrint = [];
     var articles = app.articleInfo.all;
     for(i = 1; i <= 10; i++  ) {
-          arrayToPrint.push(articles[startingNumber + i ])
+          arrayToPrint.push(articles[startingNumber + i ]);
           }
-    
+
     return arrayToPrint;
-
-
 
   }
 
--- app/controllers/applicationController.js
+++ app/controllers/applicationController.js
@@ -17,9 +17,18 @@ $(document).ready(function(){
           numberDisplayed = numberDisplayed + 10;
       }
 
-      else
-      {  // dyanimcally load from other file.
+      else if (numberDisplayed < 59)
+      {
+      app.articleInfo.adapter.loadSecondFile().then(function(data){
+        var arrayToPrint = app.articleInfo.adapter.getMore(numberDisplayed);
+        app.articleInfo.controller.renderMore(arrayToPrint);
+                numberDisplayed = numberDisplayed + 10;
+        });
       }
+      else {
+        // change button
+      }
+
    })
 
 
